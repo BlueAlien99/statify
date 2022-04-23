735189364f819eb386e8321b9c2cdd66d36ce347
BlueAlien99 <45181394+BlueAlien99@users.noreply.github.com>
2022-04-23 12:06:09 +0200

    [Working] Main widget cleaned up

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide ConnectionState;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:spotify_sdk/spotify_sdk.dart';
+import 'package:statify/connector.dart';
+import 'package:statify/dialog_manager.dart';
+import 'package:statify/screens/library_screen.dart';
+import 'package:statify/screens/search_screen.dart';
+import 'package:statify/screens/track_screen.dart';
 import 'package:statify/widgets/navbar.dart';
+
+import 'dart:async';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
@@ -11,65 +17,66 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Statify',
       theme: ThemeData(
+        brightness: Brightness.dark,
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   int _currentViewIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  static const List<Widget> _screens = [TrackScreen(), SearchScreen(), LibraryScreen()];
+
+  @override
+  void initState() {
+    super.initState();
+    Connector().subscribeConnectionState().listen((event) {
+      DialogManager().popAll(context);
+      switch (event) {
+        case ConnectionState.initializing:
+          showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: DialogManager().connectingDialogBuilder);
+          break;
+        case ConnectionState.error:
+          showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: DialogManager().connectionErrorDialogBuilder);
+          break;
+        default:
+          break;
+      }
     });
+    Connector().init();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
-          child: ListView(
-            children: [Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-              ],
-            ),
-          ]),
+        child: Column(
+          children: [
+            _screens.elementAt(_currentViewIndex),
+          ],
         ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
       extendBody: true,
       bottomNavigationBar: Navbar(
